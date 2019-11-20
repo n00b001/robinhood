743830059dcfc60fdeed9e8bdcc6f02bb3b74873
alex fanthome <alex.fanthome@thomsonreuters.com>
2019-11-20 20:35:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import time
+from multiprocessing.pool import ThreadPool
+from pprint import pprint
+
+from util import setup_session, BATCH_SIZE, generator_emails, REFERAL_CODE, BATCHES, get_new_identity
+
+
+def main():
+    while True:
+        get_new_identity()
+        with ThreadPool(processes=BATCHES) as p:
+            p.map(send_batch, range(BATCHES))
+
+
+def send_batch(_):
+    with ThreadPool(processes=BATCH_SIZE) as p:
+        statuses = p.map(process_email, generator_emails())
+    merged = {k: v for x in statuses for k, v in x.items() if v == "error"}
+    if len(list(merged.keys())) > 0:
+        pprint(merged, indent=4)
+
+
+def process_email(email):
+    try:
+        print(email)
+        s = setup_session()
+        # resp = s.get(f"https://uk.robinhood.com/{REFERAL_CODE}")
+        # if not resp.ok:
+        #     return {email: "error"}
+        # resp = s.options("https://api.robinhood.com/midlands/tailgate/devonshire/spot/")
+        # if not resp.ok:
+        #     return {email: "error"}
+        post_data = {
+            "email": email,
+            "referral_code": REFERAL_CODE
+        }
+        sleep_time = 1e-14
+        while True:
+            resp = s.post("https://api.robinhood.com/midlands/tailgate/devonshire/spot/", data=post_data)
+            if int(resp.status_code) != 429:
+                break
+            time.sleep(sleep_time)
+            sleep_time *= 2.0
+        if not resp.ok:
+            return {email: "error"}
+        # resp = s.get(f"https://api.robinhood.com/midlands/tailgate/devonshire/spot/{email}/")
+        # if not resp.ok:
+        #     return {email: "error"}
+        return {email: "ok"}
+    except:
+        return {email: "error"}
+
+
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,91 @@
+import json
+import random
+import string
+
+import requests
+from stem import Signal
+from stem.control import Controller
+
+BATCH_SIZE = 100
+BATCHES = 5
+REFERAL_CODE = "CHMX"
+
+# TO Request URL with SOCKS over TOR
+PROXY = 'socks5://localhost:9050'
+
+with open("male.txt") as f:
+    names = f.readlines()
+    males = [str(m).strip().title() for m in names]
+
+with open("female.txt") as f:
+    names = f.readlines()
+    females = [str(m).strip().title() for m in names]
+
+with open("last.txt") as f:
+    names = f.readlines()
+    lasts = [str(m).strip().title() for m in names]
+
+
+def generator_emails():
+    joined = males + females
+    random.shuffle(joined)
+    random.shuffle(lasts)
+    for fname in joined[:BATCH_SIZE]:
+        for lname in lasts[:BATCH_SIZE]:
+            chars = "".join([random.choice(string.ascii_letters) for _ in range(2)])
+            yield f"{fname}{chars}{lname}@gmail.com"
+
+    # for _ in range(BATCH_SIZE):
+    #     yield "".join([random.choice(string.ascii_letters) for _ in range(32)]) + "@gmail.com"
+
+    # word = "alexanderfanthome"
+    # # word = string.ascii_lowercase
+    # for prod in product((".", ""), repeat=len(word) - 1):
+    #     mixed = [char for tpl in zip(word, prod + ("",)) for char in tpl]
+    #     new_word = "".join(mixed)
+    #     yield f"{new_word}@gmail.com"
+
+
+def setup_session():
+    s = requests.Session()
+    s.headers = {
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36"
+    }
+    # s.proxies = {
+    #     'all_proxy': PROXY,
+    #     'http': PROXY,
+    #     'https': PROXY
+    # }
+    return s
+
+
+def get_new_identity():
+    with Controller.from_port(port=9051) as controller:
+        controller.authenticate()
+        controller.signal(Signal.NEWNYM)
+
+
+def get_current_ip():
+    session = requests.session()
+
+    session.proxies = {
+        'all_proxy': PROXY,
+        'http': PROXY,
+        'https': PROXY
+    }
+
+    try:
+        r = session.get('http://httpbin.org/ip')
+    except Exception as e:
+        print(str(e))
+    else:
+        return json.loads(r.text)["origin"].split(", ")[0]
+
+
+if __name__ == '__main__':
+    for _ in range(5):
+        print(get_current_ip())
+        get_new_identity()
